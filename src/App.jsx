efda4e7b8a39e97c6f57e24494cbb769f9109089
Sejal Rajput <sejalrana15@gmail.com>
2023-06-11 21:20:00 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { About, Contact, Experience,Certifications, Hero, Navbar, Tech, Works, S
 const App = () => {
    
   return (
-    <HashRouter base="/">
+    <HashRouter base="https://sejalrana.github.io/Portfolio/">
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
